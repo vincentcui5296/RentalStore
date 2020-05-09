3e6089bb01aabd427a8febb37a74726d830a2d42
vincentcui5296 <vincentcui5296@gmail.com>
2020-05-09 11:17:58 -0600

    Add Customer to InitialModel.

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using MovieRentalStore.Models;
 
 namespace RentalStore.Models
 {
@@ -20,6 +21,7 @@ namespace RentalStore.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
