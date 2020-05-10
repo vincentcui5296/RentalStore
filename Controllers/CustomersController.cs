e64caa51a0a6264e6d091578697e9717cce441e6
vincentcui5296 <vincentcui5296@gmail.com>
2020-05-09 23:54:03 -0600

    Query Customers data from SQL

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,6 +1,8 @@
 ﻿using MovieRentalStore.Models;
+using RentalStore.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,25 +11,28 @@ namespace MovieRentalStore.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer() { Id = 1, Name = "John Smith" },
-                new Customer() { Id = 2, Name = "Mary Williams" },
-            };
         }
 
         public ActionResult Detail(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
